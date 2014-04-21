cc80b7a0ed8a6cc0883ae531c993d94c734a8719
raposinho <a33724@alunos.isel.pt>
2014-04-21 13:05:48 +0100

    remvido um projecto que nao esta em uso

--- Serie1/SD_Serie1/Client/JobManager.cs
+++ Serie1/SD_Serie1/Client/JobManager.cs
@@ -6,9 +6,17 @@ using ClientJob;
 
 namespace Client
 {
+    [Serializable]
     class JobManager
     {
         Dictionary<Int32, IJob> dict = new Dictionary<Int32, IJob>();
 
+        public void addJob(Int32 identifier, IJob job) {
+            dict.Add(identifier, job);
+        }
+
+        public void removeJob(Int32 identifier) {
+            dict.Remove(identifier);
+        }
     }
 }
