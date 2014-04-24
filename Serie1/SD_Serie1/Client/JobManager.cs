b761544f3c27dd71b033f7023341ff1a61c1d1ff
PPedroso <pedroso_pedro@hotmail.com>
2014-04-24 10:34:23 +0100

    to discard

--- Serie1/SD_Serie1/Client/JobManager.cs
+++ Serie1/SD_Serie1/Client/JobManager.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ClientJob;
+using JobImplementation;
 
 namespace Client
 {
     class JobManager
     {
         Dictionary<Int32, IJob> dict = new Dictionary<Int32, IJob>();
+        Int32 
 
+        Job createJob(String exName,String inputFile,String outputFile){
+            Job j = new Job(exName, inputFile, outputFile);
+
+
+            return j; 
+                
+        }
+
+
+        void sendJob(Job j) { 
+            
+        }
     }
 }
