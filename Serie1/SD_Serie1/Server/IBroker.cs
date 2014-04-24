56b76c4a3f4d97e26258c4af35ddf14ef6209974
a33404 <a33404@alunos.isel.ipl.pt>
2014-04-24 13:50:05 +0100

    Draft de interfaces

---
+++ Serie1/SD_Serie1/Server/IBroker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+   interface IBroker
+   {
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <param name="jobId"></param>
+       /// <returns>Whether the job is still being processed</returns>
+       bool RequestJobStatus(int jobId);
+       int SubmitJob(JobForm jf);
+   }
+}
