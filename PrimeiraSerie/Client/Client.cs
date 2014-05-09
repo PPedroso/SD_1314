f238d1567fd59dcc69bd192a3a156c4e1efd4750
raposinho <a33724@alunos.isel.pt>
2014-05-09 23:00:46 +0100

    Adicionada possivel solução para o manager de jobs no cliente

--- PrimeiraSerie/Client/Client.cs
+++ PrimeiraSerie/Client/Client.cs
@@ -1,12 +1,74 @@
 ﻿using BrokerSAO;
 using JobImplementation;
 using System;
+using System.Collections.Generic;
 using System.Runtime.Remoting;
+using System.Threading;
 
 namespace Client
 {
     class Client
     {
+        class JobManager
+        {
+            private bool waitingEnd = false;
+            private Object lockObj = new object();
+            private Dictionary<long, Job> dict = new Dictionary<long,Job>();
+            //in case a job ended before it was added on the dictionary
+            private LinkedList<long> listEndedBeforeAddingJob = new LinkedList<long>();
+
+            public void addJob(Job j) {
+                lock(lockObj) {
+                    //verify if it's already over
+                    LinkedListNode<long> node = listEndedBeforeAddingJob.Find(j.getJobId());
+                    if (node != null) {
+                        listEndedBeforeAddingJob.Remove(node);
+                        verifyIfShouldEnd();
+                    } else {
+                        dict.Add(j.getJobId(), j);
+                    }
+                }
+            }
+
+            public void removeJob(long id) {
+                lock (lockObj) {
+                    if (dict.ContainsKey(id)) {
+                        dict.Remove(id);
+                        verifyIfShouldEnd();
+                    } else {
+                        listEndedBeforeAddingJob.AddLast(id);
+                    }
+                }
+            }
+
+            private void verifyIfShouldEnd() {
+                if (dict.Count == 0 && listEndedBeforeAddingJob.Count == 0) {
+                    Monitor.Pulse(lockObj);
+                }
+            }
+
+            public bool waitJobFinish(int timeout) {
+                lock(lockObj) {
+                    waitingEnd = true;
+                    do {
+                        if (dict.Count == 0) {
+                            return true;
+                        } 
+                        //ver timeout correctamente
+                        if (timeout == 0) {
+                            waitingEnd = false;
+                            return false;
+                        }
+                        try {
+                            Monitor.Wait(lockObj, timeout);
+                        } catch (ThreadInterruptedException e) {
+                            Thread.CurrentThread.Interrupt();
+                        }    
+                    } while(true);
+                }
+            }
+        }
+        
         class clientEndJob : MarshalByRefObject, IEndJob
         {
             public void finish(long jobId)
@@ -24,20 +86,16 @@ namespace Client
             WellKnownClientTypeEntry[] entries = RemotingConfiguration.GetRegisteredWellKnownClientTypes();
             IBrokerSAO brokerProxy = (IBrokerSAO)Activator.GetObject(entries[0].ObjectType, entries[0].ObjectUrl);
 
+            //create jobManager
+            JobManager jobManager = new JobManager();
+
             //Submissão do trabalho ao broker
             Job j = new Job("service.exe", "inputFile1", "inputFile2", "clientEndPoint", new clientEndJob());
             long id = brokerProxy.SubmitJob(j);
-            
-            Console.ReadLine();
-            //Registo do canal
-            //TcpChannel ch = new TcpChannel(0);
-            //ChannelServices.RegisterChannel(ch, false);
+            j.setJobId(id);
+            jobManager.addJob(j);
 
-            ////Criação do proxy para o objecto do broker
-            //IMyBrokerCAO brokerProxy = (IMyBrokerCAO)Activator.GetObject(typeof(IMyBrokerCAO), "tcp://localhost:1234/" + SERVER_EP);
-
-            
-            //Console.WriteLine("Job has completed: " + brokerProxy.RequestJobStatus(j.getJobId()));            
+            Console.ReadLine();         
         }
     }
 }
