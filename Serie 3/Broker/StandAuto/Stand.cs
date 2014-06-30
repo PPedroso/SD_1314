6033c2528af3794fe9bf456ade66f976d437a188
raposinho <a33724@alunos.isel.pt>
2014-06-30 22:35:09 +0100

    modificao para debug

--- Serie 3/Broker/StandAuto/Stand.cs
+++ Serie 3/Broker/StandAuto/Stand.cs
@@ -17,14 +17,17 @@ namespace StandAuto
 
         public void queryByBrand(string client, String brand) {
             IEnumerable<Car> result = carsSingleton.getCarsWithBrand(brand);
+            foreach(Car c in result) { Console.WriteLine(c.getBrand()); }
         }
 
         public void queryByMaxPrice(string client, int maxPrice) {
             IEnumerable<Car> result = carsSingleton.getCarsWithPriceLowerThen(maxPrice);
+            foreach (Car c in result) { Console.WriteLine(c.getPrice()); }
         }
 
         public void queryByMinimumYearRegistration(string client, int minYearRegistration) {
             IEnumerable<Car> result = carsSingleton.getCarsWithYearRegistrationHigherThen(minYearRegistration);
+            foreach (Car c in result) { Console.WriteLine(c.getYearRegistration()); }
         }
     }
 }
