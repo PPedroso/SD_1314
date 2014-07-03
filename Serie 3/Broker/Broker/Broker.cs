e0f2b2cd05538f57e5efc0e3f14bb495ca990ffc
raposinho <a33724@alunos.isel.pt>
2014-07-03 20:57:13 +0100

    adicionada concorrencia nos stands do broker

--- Serie 3/Broker/Broker/Broker.cs
+++ Serie 3/Broker/Broker/Broker.cs
@@ -14,39 +14,69 @@ namespace BrokerInterface
 {
     public class Broker
     {
-        private LinkedList<IStandBrokerContract> stands = new LinkedList<IStandBrokerContract>();
+        private readonly Object lockObj = new Object();
+        private List<IStandBrokerContract> stands = new List<IStandBrokerContract>();
 
         public void addStand(IStandBrokerContract proxy) {
             Console.WriteLine(String.Format("Broker adding stand: {0}",proxy.ToString()));
-            stands.AddLast(proxy);
+            lock (lockObj) {
+                stands.Add(proxy);
+            }
         }
 
         public void broadcastBrandQuery(string client, string brand) { 
-            stands.ForEach(stand => stand.queryByBrand(client, brand)); 
+            broadcast(client, stand => {
+                try {
+                    stand.queryByBrand(client, brand);
+                    return false;
+                } catch (Exception e) {
+                    Console.WriteLine("Removing one of the stands, since it failed to propagate brand query");
+                    return true;
+                }
+            }); 
         }
 
         public void broadcastYearQuery(string client, int year) {
-            stands.ForEach(stand => stand.queryByMinimumYearRegistration(client, year)); 
+            broadcast(client, stand => {
+                try {
+                    stand.queryByMinimumYearRegistration(client, year);
+                    return false;
+                } catch (Exception e) {
+                    Console.WriteLine("Removing one of the stands, since it failed to propagate year query");
+                    return true;
+                }
+            });
         }
 
         public void broadcastPriceQuery(string client, int price) {
-            stands.ForEach(stand => stand.queryByMaxPrice(client, price)); 
+            broadcast(client, stand => {
+                try {
+                    stand.queryByMaxPrice(client, price);
+                    return false;
+                } catch (Exception e) {
+                    Console.WriteLine("Removing one of the stands, since it failed to propagate price query");
+                    return true;
+                }
+            });
         }
 
-        static void Main(string[] args)
-        {
+        private void broadcast(string client, Func<IStandBrokerContract, bool> func) {
+            lock(lockObj) {
+                for (int i = 0; i < stands.Count; i++) {
+                    if (func(stands[i])) {
+                        //in case it needs to be removed
+                        stands.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args) {
             using (ServiceHost host = new ServiceHost(typeof(BrokerServices))) {
                 host.Open();
                 Console.WriteLine("Broker service started, press any key to shut down");
                 Console.ReadLine();
             }
-        }
-
-    }
-
-    public static class MyExtensions {
-        public static void ForEach<T>(this IEnumerable<T> @this, Action<T> action) {
-            foreach (var x in @this) { action(x); }
         }
     }
 }
