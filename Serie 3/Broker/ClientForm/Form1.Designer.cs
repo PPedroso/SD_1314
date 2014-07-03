c13db89c807aef0d42eb0b81d3706da57d3c93d0
raposinho <a33724@alunos.isel.pt>
2014-07-03 18:11:21 +0100

    modificada form

--- Serie 3/Broker/ClientForm/Form1.Designer.cs
+++ Serie 3/Broker/ClientForm/Form1.Designer.cs
@@ -32,50 +32,73 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Location = new System.Drawing.Point(170, 50);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Query";
+            this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 12);
+            this.textBox3.Location = new System.Drawing.Point(12, 50);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(145, 20);
             this.textBox3.TabIndex = 3;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(118, 12);
+            this.listBox1.Location = new System.Drawing.Point(251, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(476, 134);
+            this.listBox1.Size = new System.Drawing.Size(481, 134);
             this.listBox1.TabIndex = 6;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 67);
+            this.button2.Location = new System.Drawing.Point(12, 99);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 47);
+            this.button2.Size = new System.Drawing.Size(233, 47);
             this.button2.TabIndex = 7;
             this.button2.Text = "Accept Proposal";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Brand",
+            "Price",
+            "Year of Registration"});
+            this.comboBox1.Location = new System.Drawing.Point(89, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Search by";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(599, 153);
+            this.ClientSize = new System.Drawing.Size(744, 153);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox3);
@@ -93,6 +116,8 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Serie 3/Broker/ClientForm/Form1.cs
+++ Serie 3/Broker/ClientForm/Form1.cs
@@ -22,7 +22,9 @@ namespace ClientForm
     
     public partial class Form1 : Form
     {
-
+        const string QUERY_BY_BRAND = "Brand";
+        const string QUERY_BY_PRICE = "Price";
+        const string QUERY_BY_YEAR = "Year of Registration";
         const string CLIENT_SERVICE_ENDPOINT = "http://localhost:8010/ClientService";
         EndpointAddress addr = new EndpointAddress("http://localhost:8080/BrokerClientService");
         BasicHttpBinding bind = new BasicHttpBinding();
@@ -30,22 +32,17 @@ namespace ClientForm
         public static Form _Form1;
         static ServiceHost svcHost;
 
-
-        private static void RunService()
-        {
+        private static void RunService() {
             Uri addr = new Uri(CLIENT_SERVICE_ENDPOINT);
             BasicHttpBinding bind = new BasicHttpBinding();
             svcHost = new ServiceHost(typeof(ClientService));
 
             ServiceMetadataBehavior smb = svcHost.Description.Behaviors.Find<ServiceMetadataBehavior>();
-            if (smb != null)
-            {
+            if (smb != null) {
                 smb.HttpGetEnabled = true;
                 smb.HttpGetUrl = addr;
 
-            }
-            else
-            {
+            } else {
                 smb = new ServiceMetadataBehavior();
                 smb.HttpGetEnabled = true;
                 smb.HttpGetUrl = addr;
@@ -57,8 +54,7 @@ namespace ClientForm
 
         }
 
-        protected override void OnFormClosed(FormClosedEventArgs e)
-        {
+        protected override void OnFormClosed(FormClosedEventArgs e) {
             base.OnFormClosed(e);
             svcHost.Close();
         }
@@ -68,31 +64,37 @@ namespace ClientForm
             listBox1.Items.Add(p);
         }
 
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
             _Form1 = this;
             IChannelFactory<IBrokerClientService> cfact = new ChannelFactory<IBrokerClientService>(bind);
             proxy = cfact.CreateChannel(addr);
             RunService();
-           
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                proxy.submitQueryByBrand(CLIENT_SERVICE_ENDPOINT, textBox3.Text);
+        private void button1_Click(object sender, EventArgs e) {
+            listBox1.Text = "";
+            try {
+                int n;
+                string queryType = comboBox1.Text;
+                switch (queryType) {
+                    case QUERY_BY_BRAND:
+                        proxy.submitQueryByBrand(CLIENT_SERVICE_ENDPOINT, textBox3.Text);
+                        break;
+                    case QUERY_BY_PRICE:
+                        if (int.TryParse(textBox3.Text, out n)) {
+                            proxy.submitQueryByMaxPrice(CLIENT_SERVICE_ENDPOINT, n);
+                        }
+                        break;
+                    case QUERY_BY_YEAR:
+                        if (int.TryParse(textBox3.Text, out n)) {
+                            proxy.submitQueryByMinumumYearRegistration(CLIENT_SERVICE_ENDPOINT, n);
+                        }
+                        break;
+                }
+            } catch (Exception) {
+                listBox1.Text = "Failed to contact the necessary services";
             }
-            catch (Exception ex)
-            {
-                String str = ex.Message;
-            }
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -107,22 +109,11 @@ namespace ClientForm
 
             proxy = cfact.CreateChannel(addr);
 
-
             try { 
                 proxy.reserveCar(p.id);
+            } catch(FaultException<StandClientContract.AlreadyReservedFault> ex) {
+                MessageBox.Show(String.Format("Car with id : {} is already reserved", ex.Detail.Id));
             }
-            catch(FaultException<StandClientContract.AlreadyReservedFault> ex)
-            {
-                MessageBox.Show("Already reserved");
-            }
-            
-
-            
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-           
         }
     }
 }
