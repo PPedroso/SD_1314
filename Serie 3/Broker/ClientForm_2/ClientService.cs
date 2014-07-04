be812a23ac3d34a22b39c94f0f91426fc6553310
PPedroso <pedroso_pedro@hotmail.com>
2014-07-04 01:54:13 +0100

    Não consigo por dois clients ou dois stands a funcionar ao mesmo tempo. N sei porque ele n detecta que mudei a porta no app.config

---
+++ Serie 3/Broker/ClientForm_2/ClientService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using ClientContract;
+
+namespace ClientForm
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class ClientService:IClient
+    {
+        public void submitProposal(string endpoint,int id ,string brand,int year, int price)
+        {
+
+            Proposal p = new Proposal(id,brand,endpoint,year,price);
+            ((Form1)Form1._Form1).updateTextBox(p);
+        }
+    }
+}
